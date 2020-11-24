3129ae7aab2b6258279f29d9ee7e1f82af465229
Kali Linux <kali@ip-172-31-47-49.ap-northeast-2.compute.internal>
2020-11-24 23:25:03 +0000

    [Tested] Change Log

--- server.js
+++ server.js
@@ -1,9 +1,31 @@
 var express     = require('express'),
     app         = express();
     bodyParser = require ("body-parser")
- 
+    const server    = require('http').createServer(app);
+const io        = require('socket.io')(server);
+
+const exec = require('child_process').exec;
+
+const isRunning = (query, cb) => {
+    let platform = process.platform;
+    let cmd = '';
+	console.log("isRunning : "+platform);
+	cmd = `pgrep ${query}`;
+    /*switch (platform) {
+        case 'win32' : cmd = `tasklist`; break;
+        case 'darwin' : cmd = `ps -ax | grep ${query}`; break;
+        case 'linux' : cmd = `pgrep ${query}`; break;
+        default: break;
+    }*/
+    exec(cmd, (err, stdout, stderr) => {
+		console.log(stdout);
+        cb(stdout);
+    });
+}
+
+
 //web 폴더 밑에 있는 파일들을 요청이 있을때 접근 가능하도록 합니다.
-//app.use(express.static(__dirname + '/web')); 
+app.use(express.static(__dirname + '/web')); 
 //app.use(bodyParser.json());
  
 // 유저가 root 를 요청 했을 때, index.html 파일을 전송합니다.
@@ -15,6 +37,32 @@ app.get('/', function(req,res){
     res.sendfile('web/index.html');
 });
  
+app.get('/isOn', function(req,res){
+	isRunning('ruby', (status) => {
+		res.send(status);
+	})
+});
+ 
+app.get('/powerOn', function(req,res){
+	exec(`killall ruby`, (err, stdout, stderr) => {
+		console.log(stdout);
+		if(stdout ="") log = "Shutdown Process is stopped."
+		else log = stdout;
+		io.emit('log',log);
+    });
+	res.send(200);
+});
+ 
+app.get('/powerOff', function(req,res){
+	var child = require('child_process').execFile('shell/remote_shutdown.sh'); 
+	// use event hooks to provide a callback to execute when data are available: 
+	child.stdout.on('data', function(data) {
+		console.log(data.toString()); 
+		io.emit('log',data.toString());
+	});
+	res.send(200);
+});
+ 
 //다른 경로를 요청했을때, 실제 그 경로에 있는 파일을 전달합니다.
 /*app.get('/*', function(req, res) { 
  
@@ -24,7 +72,15 @@ app.get('/', function(req,res){
  });
 });
  */
-app.listen(80); //1024 이하의 포트는 특정 cap 권한이 필요합니다.
+
+
+io.on('connection', (socket) => {
+	console.log();
+	
+});
+
+
+server.listen(80); //1024 이하의 포트는 특정 cap 권한이 필요합니다.
 
 
 ///////////////////////////////////////////////////////////////////
--- shell/remote_shutdown.sh
+++ shell/remote_shutdown.sh
@@ -1,3 +1,4 @@
 #if pgrep msfconsole; then pkill msfconsole; fi
 killall ruby || echo "Process was not running."
+echo "Ready to run"
 msfconsole -r msf_shutdown.rc
